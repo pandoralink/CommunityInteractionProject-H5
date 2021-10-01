b5c37dc99fd657cc0de373b9f05e67c4b12db616
poplink <2219535001@qq.com>
2021-10-01 19:56:55 +0800

    补充

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <new-edit />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NewEdit from './components/NewEdit.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NewEdit
   }
 }
 </script>
@@ -23,6 +22,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
 }
 </style>
