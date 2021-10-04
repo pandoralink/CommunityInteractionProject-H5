12d328305bd0a65e1a06b60a90a5ca9ba4d5118d
poplink <2219535001@qq.com>
2021-10-04 23:17:04 +0800

    1. 文章创建 2. 文章更新

--- src/components/NewEdit.vue
+++ src/components/NewEdit.vue
@@ -11,12 +11,19 @@
       <span @click="closeInfo">❌</span>
     </div>
     <div id="demo1"></div>
+    <button class="article-post" @click="articlePost">发表</button>
   </div>
 </template>
 
 <script>
 // 引入 wangEditor
 import wangEditor from "wangeditor";
+import axios from "../../../journal-study/src/utils/axios.js";
+import config from "../config/config.js";
+/**
+ * coverUrl 默认为空，暂不考虑编辑器
+ * 编辑图片
+ */
 export default {
   data() {
     return {
@@ -24,7 +31,14 @@ export default {
       editorData: "",
       infoFlag: true,
       title: "",
+      coverUrl: "",
+      uid: 0,
+      articleName: "",
+      aid: 0,
     };
+  },
+  created() {
+    this.initData();
   },
   mounted() {
     const editor = new wangEditor(`#demo1`);
@@ -48,6 +62,7 @@ export default {
     ];
     // 创建编辑器
     editor.create();
+    editor.$textElem.css("text-align", "left");
     this.editor = editor;
   },
   methods: {
@@ -57,6 +72,80 @@ export default {
     },
     closeInfo() {
       this.infoFlag = false;
+    },
+    articlePost() {
+      if (this.articleName && this.aid) {
+        this.articleUpdate();
+        return;
+      }
+      const content = {
+        uid: this.uid,
+        title: this.title,
+        coverUrl: this.coverUrl,
+        html: this.editor.txt.html(),
+      };
+      axios
+        .get(config.baseUrl + "/addArticle", { params: content })
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      this.title = "";
+      this.editor.txt.clear();
+    },
+    articleUpdate() {
+      const content = {
+        title: this.title,
+        articleName: this.articleName,
+        coverUrl: this.coverUrl,
+        aid: this.aid,
+        html: this.editor.txt.html(),
+      };
+      // console.log(this.articleName);
+      axios
+        .get(config.baseUrl + "/updateArticle", { params: content })
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    initData() {
+      this.uid = config.getQueryVariable("uid")
+        ? config.getQueryVariable("uid")
+        : 0;
+      this.articleName = config.getQueryVariable("articleName")
+        ? config.getQueryVariable("articleName")
+        : "";
+      this.articleName = /\/([0-9]+\.html)/.exec(this.articleName)[1];
+      this.aid = config.getQueryVariable("aid")
+        ? config.getQueryVariable("aid")
+        : 0;
+      this.title = config.getQueryVariable("title")
+        ? decodeURI(config.getQueryVariable("title"))
+        : "";
+      if (this.articleName && this.aid && this.title) {
+        const content = {
+          articleName: this.articleName,
+        };
+        axios
+          .get(config.baseUrl + "/getArticleHtml", { params: content })
+          .then((res) => {
+            if (this.editor) {
+              // 如果这时候 wangeditor 已经创建
+              this.editor.txt.html(res.data.content);
+            } else {
+              const defaultDom = document.querySelector("#demo1");
+              defaultDom.innerHTML = res.data.content;
+            }
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
     },
   },
   beforeDestroy() {
@@ -97,6 +186,41 @@ export default {
 }
 .article-title:focus {
   outline: none;
+}
+.article-post {
+  display: flex;
+  position: fixed;
+  right: 10px;
+  bottom: 20%;
+  background: var(--selectColor);
+  color: white;
+  /* wangeditor 的 z-index 是 10000 */
+  z-index: 10001;
+  padding: 0px 16px;
+  border: 0px;
+  height: 32px;
+  align-items: center;
+}
+button {
+  height: 3em;
+  align-self: flex-end;
+  border: 3px solid black;
+  border-radius: 10px;
+  padding: 5px 10px;
+  margin: 5px;
+  font-weight: 500;
+  font-size: 14px;
+  background-color: #ffffff;
+  cursor: pointer;
+  background: #fff;
+  text-align: center;
+  outline: 0;
+}
+button:active {
+  /* 主要面向移动端，无需设置 hover */
+  color: var(--infoColor);
+  box-shadow: 1px 1px 4px 2px rgba(0, 0, 0, 0.1);
+  outline: 0;
 }
 @media screen and (max-width: 900px) {
   .edit-info {
---
+++ src/config/config.js
@@ -0,0 +1,31 @@
+const config = {};
+config.defaultHeadBaseUrl = "http://116.63.152.202:5002/userHead/";
+config.baseUrl = "http://116.63.152.202:3002";
+// config.baseUrl = "http://localhost:3000"
+config.localBaseUrl = "http://localhost:3000";
+config.getQueryVariable = function getQueryVariable(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    if (pair[0] == variable) {
+      return pair[1];
+    }
+  }
+  return false;
+};
+/**
+ * 测试链接
+ * 本地
+ * http://127.0.0.1:5501/news-create-manager/News/demo.html?userName="庞老闆"&userId=1005&newId=34&userHeadUrl="http://116.63.152.202:5002/userHead/default_head.png"
+ * 远程
+ * http://116.63.152.202:5002/News/3.html?userName="庞老闆"&userId=1005&newId=34&userHeadUrl="http://116.63.152.202:5002/userHead/default_head.png"
+ * 本地更新链接
+ * http://127.0.0.1:8080/?articleName="http://116.63.152.202:5002/News/273.html"&aid=275
+ * 远程更新链接
+ * http://116.63.152.202:5002/?articleName="http://116.63.152.202:5002/News/273.html"&aid=275
+ * 远程新增文章链接
+ * http://116.63.152.202:5002/?uid=1005
+ */
+
+export default config;
